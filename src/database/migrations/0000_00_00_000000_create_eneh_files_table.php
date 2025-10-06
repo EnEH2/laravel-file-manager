2d255787a4a4779fb55dfff01ca42bd4367dc993
emon233 <sazid.hossain.1433@gmail.com>
2025-10-06 23:33:25 +0600

    migration added

--- src/database/migrations/0000_00_00_000000_create_eneh_files_table.php
+++ src/database/migrations/0000_00_00_000000_create_eneh_files_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('eneh_files', function (Blueprint $table) {
             $table->id();
+            $table->string('uuid')->unique();
+            $table->string('name');
+            $table->string('path');
+            $table->string('mime_type');
+            $table->string('extension');
+            $table->unsignedBigInteger('size');
+            $table->string('disk');
+            $table->boolean('visibility')->default(true);
+            $table->string('model_type')->nullable();
+            $table->string('model_id', 100)->nullable();
+            $table->string('auth_type')->nullable();
+            $table->string('auth_id', 100)->nullable();
             $table->timestamps();
         });
     }
