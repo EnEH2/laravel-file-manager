925fe3bc46c438bac1e856183f2eb0f49b9778c3
emon233 <sazid.hossain.1433@gmail.com>
2025-10-06 23:53:12 +0600

    Model modified

--- src/Models/EnehFile.php
+++ src/Models/EnehFile.php
@@ -6,5 +6,61 @@ use Illuminate\Database\Eloquent\Model;
 
 class EnehFile extends Model
 {
-    //
+    /**
+     * Boot function
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (EnehFile $enehFile) {
+            $enehFile->uuid = (string) \Str::uuid();
+        });
+    }
+
+
+    /**
+     * Fillable columns
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'name',
+        'path',
+        'mime_type',
+        'extension',
+        'size',
+        'disk',
+        'visibility',
+        'model_type',
+        'model_id',
+        'auth_type',
+        'auth_id',
+        'deleted',
+    ];
+
+
+    /**
+     * Relation with Related Model
+     *
+     * @return MorphTo
+     */
+    public function fileable()
+    {
+        return $this->morphTo();
+    }
+
+
+    /**
+     * Relation with Creator
+     *
+     * @return MorphTo
+     */
+    public function authenticable()
+    {
+        return $this->morphTo();
+    }
 }
