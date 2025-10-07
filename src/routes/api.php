1e0013f59f2a03b459f8fca2ca9598a786b5bab1
emon233 <sazid.hossain.1433@gmail.com>
2025-10-07 16:00:38 +0600

    changes

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::name('eneh-filemanager')->prefix('/eneh-filemanager')->group(function () {
-    Route::post('/upload', [FileController::class, 'store'])->name('upload');
+    Route::post('/upload', [FileController::class, 'upload'])->name('upload');
 });
 
